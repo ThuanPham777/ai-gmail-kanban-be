8f584c745d08be91961104ca958b6a6cd494d42a
22120358 <phamchithuan777@gmail.com>
2025-11-19 21:15:52 +0700

    update login and register dto

--- src/users/dtos/login.dto.ts
+++ src/users/dtos/login.dto.ts
@@ -1,10 +1,13 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
   email: string;
 
   @IsString()
-  @MinLength(6)
+  @MinLength(8)
+  @Matches(/^(?=.*[A-Z])(?=.*\d).+$/, {
+    message: 'Password must include at least one uppercase letter and one number',
+  })
   password: string;
 }
